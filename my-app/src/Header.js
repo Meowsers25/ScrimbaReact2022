3f0e27a9ba4ec03088e7be0f9a9b0b97fea98f1e
Christian Lamothe <c.lamothe25@gmail.com>
2022-03-22 16:08:41 -0400

    Finished initial project with components and styling

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import reactLogo from "../src/images/react.png"
 
 function Header(){
     return(
             <header>
                 <nav className="nav-bar">
-                    <img className="react-img" src="./react.png" alt="React logo" />
+                    <img className="react-img" src={reactLogo} alt="React logo" />
                     <ul className="nav-items">
                         <li>Pricing</li>
                         <li>About</li>
