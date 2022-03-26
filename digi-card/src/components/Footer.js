e5df12794b5e8f9e6a448859a952b0882a0d510f
Christian Lamothe <c.lamothe25@gmail.com>
2022-03-26 07:44:32 -0400

    Finished solo project - digi card

--- digi-card/src/components/Footer.js
+++ digi-card/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Facebook from '../images/Facebook Icon.png'
+import GitHub from '../images/GitHub Icon.png'
+import Instagram from '../images/Instagram Icon.png'
+import Twitter from '../images/Twitter Icon.png'
 
 function Footer(){
     return(
         <div className="footer">
-            <h1>Footer</h1>
+            <img className="icon" src={ Twitter } alt="twitter png" />
+            <img className="icon left" src={ Facebook } alt="facebook png" />
+            <img className="icon left" src={ Instagram } alt="instagram png" />
+            <img className="icon left" src={ GitHub } alt="github png" />
         </div>
     )
 }
--- digi-card/src/components/Info.js
+++ digi-card/src/components/Info.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Me from "../images/me.png"
+import Mail from "../images/Mail.png"
+import Linkedin from "../images/linkedin.png"
 
 function Info(){
     return(
@@ -8,8 +10,8 @@ function Info(){
             <h2 className="name">Christian Lamothe</h2>
             <h3 className="fd">Frontend Developer</h3>
             <h4 className="web">lamothe.development.com</h4>
-            <button className="email">Email</button>
-            <button className="linkedin">Linkedin</button>
+            <button className="email"><img className="mpng" src={ Mail } alt="mail png" /> Email</button>
+            <button className="linkedin"><img className="lpng" src={ Linkedin } alt="linkedin png" />Linkedin</button>
         </div>
     )
 }
