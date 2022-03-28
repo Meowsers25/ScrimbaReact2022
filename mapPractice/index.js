7a45c2cb7dac56fa2fb5b8a31de8a20c02561e24
Christian Lamothe <c.lamothe25@gmail.com>
2022-03-28 15:36:25 -0400

    .map() practice for React

---
+++ mapPractice/index.js
@@ -0,0 +1,43 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+const sqrNums = nums.map(x => x * x)
+console.log(sqrNums)
+
+/* Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const properNames = names.map(nm => nm[0].toUpperCase() + nm.slice(1))
+console.log(properNames)
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+const pTags = pokemon.map(character => {
+    return `<p>${character}</p>`
+})
+
+console.log(pTags)
+// good practice for React
+// Also - DUH! backticks!!!!
+// first 2 are implicit returns (no return keyword or curly braces)
+
