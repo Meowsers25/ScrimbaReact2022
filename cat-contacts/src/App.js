36e5680309875072caf36d6d95ee711e65dc4b24
Christian Lamothe <c.lamothe25@gmail.com>
2022-03-27 21:49:14 -0400

    Props are aewsome

--- cat-contacts/src/App.js
+++ cat-contacts/src/App.js
@@ -4,7 +4,30 @@ import Contact from './components/Contact'
 export default function App(){
   return(
     <div className="contacts">
-      <Contact />
+      <Contact 
+        img="images/cat1.jpg"
+        name="Mr Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img="images/cat2.jpg"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact 
+        img="images/cat5.jpg"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact 
+        img="images/cat4.jpg"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
     </div>
   )
 }
--- cat-contacts/src/components/Contact.js
+++ cat-contacts/src/components/Contact.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import Cat1 from '../images/cat1.jpg'
-import Mail from '../images/Mail.png'
 
-export default function Contact(){
+export default function Contact(props){
+    console.log(props)
     return(
     <div className="contact-card">
-            <img src={ Cat1 } alt="cute kitty 1"/>
-            <h3>Mr. Whiskerson</h3>
+            <img src={ props.img } alt="cute kitty 1"/>
+            <h3>{ props.name }</h3>
             <div className="info-group">
-
-                <p>(212) 555-1234</p>
+                <img src="images/phone.png" alt="phone" />
+                <p>{ props.phone }</p>
             </div>
             <div className="info-group">
-                <img src={ Mail } alt="email"/>
-                <p>mr.whiskaz@catnap.meow</p>
+                <img src="images/Mail.png" alt="email"/>
+                <p>{ props.email }</p>
             </div>
         </div>
     )
