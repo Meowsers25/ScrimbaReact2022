ea3c04d5dee950c05d42507590d64ddbbceebc9f
Christian Lamothe <c.lamothe25@gmail.com>
2022-03-28 12:24:32 -0400

    Destructuring the app

--- cat-contacts/src/components/Contact.js
+++ cat-contacts/src/components/Contact.js
@@ -1,18 +1,48 @@
 import React from 'react'
 
-export default function Contact(props){
-    console.log(props)
+// export default function Contact(props){
+//     // console.log(props)
+//     return(
+//     <div className="contact-card">
+//             <img src={ props.img } alt="cute kitty 1"/>
+//             <h3>{ props.name }</h3>
+//             <div className="info-group">
+//                 <img src="images/phone.png" alt="phone" />
+//                 <p>{ props.phone }</p>
+//             </div>
+//             <div className="info-group">
+//                 <img src="images/Mail.png" alt="email"/>
+//                 <p>{ props.email }</p>
+//             </div>
+//         </div>
+//     )
+//}
+
+// destructuring
+// const person = {
+//     img: "./images/mr-whiskerson.png",
+//     name: "Mr. Whiskerson",
+//     phone: "(800) 555-1234",
+//     email: "mr.whiskaz@catnap.meow"
+// }
+// const {img, name, phone} = person
+// console.log(name, phone)
+
+
+//Here is the component destructured
+export default function Contact({img, name, phone, email}){
+    // console.log(props)
     return(
     <div className="contact-card">
-            <img src={ props.img } alt="cute kitty 1"/>
-            <h3>{ props.name }</h3>
+            <img src={ img } alt="cute kitty 1"/>
+            <h3>{ name }</h3>
             <div className="info-group">
                 <img src="images/phone.png" alt="phone" />
-                <p>{ props.phone }</p>
+                <p>{ phone }</p>
             </div>
             <div className="info-group">
                 <img src="images/Mail.png" alt="email"/>
-                <p>{ props.email }</p>
+                <p>{ email }</p>
             </div>
         </div>
     )
