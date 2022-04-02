6422b839d23ed2571c7dafe9bd0a669a510e96ec
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-01 22:23:49 -0400

    more work on travel journal

--- travel-journal/src/components/Entry.js
+++ travel-journal/src/components/Entry.js
@@ -2,8 +2,10 @@ export default function Entry(props){
     return(
         <div className="travel-entry">
             <img className="country-pic" src={props.imageUrl} alt="beautiful pic" />
-            <h3>{props.title}</h3>
-            <h3>{props.location}</h3>
+            <div className="travel-info">
+                <h3>{props.title}</h3>
+                <h3>{props.location}</h3>
+            </div>
         </div>
     )
 }
