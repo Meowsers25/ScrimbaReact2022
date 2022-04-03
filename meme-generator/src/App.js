7f42f1364e38db45af19f2613426f40789cfb605
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-03 16:16:06 -0400

    Starting header

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,7 +1,10 @@
+import Header from './components/Header'
+
 function App() {
   return (
     <div>
-      <h1>MEME GENERATOR</h1>
+      <Header />
+      
     </div>
   );
 }
---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <img className='logo' src='./images/Logo.png' alt="logo" />
+            <p className="rcp">React Course - Project 3</p>
+        </header>
+    )
+}
