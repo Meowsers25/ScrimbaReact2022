e1e8643dad3e6f1e47d7c0e3bea2deda5697e5bc
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-04 10:05:17 -0400

    Created Meme component

---
+++ meme-generator/src/components/Meme.js
@@ -0,0 +1,14 @@
+export default function Meme(){
+    return(
+        <div className='inputs'>
+            <div className='fields'>
+            <input className='top ipt' type='text'/>
+            <input className='bottom ipt' type='text'/>
+            </div>
+            <div className='pain'>
+            <button className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
+            </div>
+        </div>
+        
+    )
+}
