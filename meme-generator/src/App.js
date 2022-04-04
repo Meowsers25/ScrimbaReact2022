b1471b9a1e73629e4f1abe3b40756bb0fd28b1dc
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-04 13:19:26 -0400

    Starting importing images

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/Header'
 import Meme from './components/Meme'
+// import memesData from './memesData'
 
 function App() {
+  
   return (
     <div className='generator'>
       <Header />
       <Meme />
+      
     </div>
   );
 }
--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,14 +1,23 @@
-export default function Meme(){
+import memesData from '../memesData'
+
+export default function Meme(props){
+    function getMemeImage(){
+        const memesArray = memesData.data.memes 
+        const randNum = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randNum].url
+        console.log(url)
+    }
+    console.log()
     return(
-        <form className='inputs'>
+        <div className='inputs'>
             <div className='fields'>
             <input className='top ipt' placeholder='Top Text' type='text'/>
             <input className='bottom ipt' placeholder='Bottom Text' type='text'/>
             </div>
             <div className='pain'>
-            <button className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
+            <button onClick={getMemeImage} className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
             </div>
-        </form>
+        </div>
         
     )
 }
