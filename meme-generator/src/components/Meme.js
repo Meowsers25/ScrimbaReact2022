6177e5298c9e8c1b86165afc8e30d8b6c30d1de3
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-04 10:24:17 -0400

    Finish form portion

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,14 +1,14 @@
 export default function Meme(){
     return(
-        <div className='inputs'>
+        <form className='inputs'>
             <div className='fields'>
-            <input className='top ipt' type='text'/>
-            <input className='bottom ipt' type='text'/>
+            <input className='top ipt' placeholder='Top Text' type='text'/>
+            <input className='bottom ipt' placeholder='Bottom Text' type='text'/>
             </div>
             <div className='pain'>
             <button className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
             </div>
-        </div>
+        </form>
         
     )
 }
