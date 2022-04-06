7ac42acd629e449c514404a03916d9e6759b5616
Christian Lamothe <c.lamothe25@gmail.com>
2022-04-06 13:59:44 -0400

    Adding random image

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,23 +1,44 @@
+import React from 'react'
 import memesData from '../memesData'
 
+/**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
 export default function Meme(props){
+
+    const [memeImage, setMemeImage] = React.useState('')
+
     function getMemeImage(){
         const memesArray = memesData.data.memes 
         const randNum = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randNum].url
-        console.log(url)
+        setMemeImage(memesArray[randNum].url)
     }
-    console.log()
+    
     return(
+        
         <div className='inputs'>
+            
             <div className='fields'>
-            <input className='top ipt' placeholder='Top Text' type='text'/>
-            <input className='bottom ipt' placeholder='Bottom Text' type='text'/>
+                <input className='top ipt' placeholder='Top Text' type='text'/>
+                <input className='bottom ipt' placeholder='Bottom Text' type='text'/>
             </div>
             <div className='pain'>
-            <button onClick={getMemeImage} className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
+                <button onClick={getMemeImage} className='btn'><img className='btn-img' src='./images/btnImg.png' alt='button text' /></button>
+            </div>
+            <div className="align-img">
+                <img className="genPhoto" src={memeImage} alt="" />
             </div>
         </div>
+        
+     
         
     )
 }
